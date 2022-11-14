e67c93d07e3746775ce8e996cd5665530e5c8acc
JonGoneWrong <jongonewrong@gmail.com>
2022-11-14 14:26:04 -0600

    Config Deploy

--- appsquared_back/settings.py
+++ appsquared_back/settings.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = True if os.getenv('MODE') == 'dev' else False
 
